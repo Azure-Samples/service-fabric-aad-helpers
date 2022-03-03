1ab616ab081fdb919f7a931f4fd4d6af6a6b0360
jason gilbertson <jagilber@microsoft.com>
2022-03-03 09:36:36 -0500

    format

--- Common.ps1
+++ Common.ps1
@@ -6,12 +6,11 @@
 Common script, do not call it directly.
 #>
 
-if($headers){
+if ($headers) {
     Exit
 }
 
-Try
-{
+Try {
     # Install latest AD client library
     $nuget = "nuget.exe"
     if (!(test-path $nuget)) {
@@ -19,7 +18,7 @@ Try
         invoke-webRequest $nugetDownloadUrl -outFile $nuget
     }
 
-    $env:path = $env:path.replace(";$pwd;","") + ";$pwd;"
+    $env:path = $env:path.replace(";$pwd;", "") + ";$pwd;"
     $ADPackage = "Microsoft.IdentityModel.Clients.ActiveDirectory"
     & nuget.exe install $ADPackage > nuget.log
 
@@ -27,14 +26,12 @@ Try
     $FilePath = (Get-Item .\\Microsoft.IdentityModel.Clients.ActiveDirectory.[0-9].[0-9].[0-9]\\lib\\net[0-9][0-9]\\Microsoft.IdentityModel.Clients.ActiveDirectory.dll).FullName | Resolve-Path -Relative
     Add-Type -Path $FilePath
 }
-Catch
-{
+Catch {
     Write-Warning $_.Exception.Message
 }
 
-function GetRESTHeaders()
-{
-	# Use common client 
+function GetRESTHeaders() {
+    # Use common client 
     $clientId = "1950a258-227b-4e31-a9cf-717495945fc2"
     $redirectUrl = "urn:ietf:wg:oauth:2.0:oob"
     
@@ -49,36 +46,31 @@ function GetRESTHeaders()
     return $headers
 }
 
-function CallGraphAPI($uri, $headers, $body, $method = "Post")
-{
+function CallGraphAPI($uri, $headers, $body, $method = "Post") {
     $json = $body | ConvertTo-Json -Depth 4 -Compress
     return (Invoke-RestMethod $uri -Method $method -Headers $headers -Body $json -ContentType "application/json")
 }
 
-function AssertNotNull($obj, $msg){
-    if($obj -eq $null -or $obj.Length -eq 0){ 
+function AssertNotNull($obj, $msg) {
+    if ($obj -eq $null -or $obj.Length -eq 0) { 
         Write-Warning $msg
         Exit
     }
 }
 
 # Regional settings
-switch ($Location)
-{
-    "china"
-    {
+switch ($Location) {
+    "china" {
         $resourceUrl = "https://graph.chinacloudapi.cn"
         $authString = "https://login.partner.microsoftonline.cn/" + $TenantId
     }
     
-    "germany"
-    {
+    "germany" {
         $resourceUrl = "https://graph.cloudapi.de"
         $authString = "https://login.microsoftonline.de/" + $TenantId   
     }
 
-    default
-    {
+    default {
         $resourceUrl = "https://graph.windows.net"
         $authString = "https://login.microsoftonline.com/" + $TenantId
     }
@@ -86,8 +78,7 @@ switch ($Location)
 
 $headers = GetRESTHeaders
 
-if ($ClusterName)
-{
+if ($ClusterName) {
     $WebApplicationName = $ClusterName + "_Cluster"
     $WebApplicationUri = "https://$ClusterName"
     $NativeClientApplicationName = $ClusterName + "_Client"
