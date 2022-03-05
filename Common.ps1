b65d8b7638b377dfd8d06af15a99b1b0f8057bad
jason gilbertson <jagilber@microsoft.com>
2022-03-05 11:14:59 -0500

    tested cloudshell option

--- Common.ps1
+++ Common.ps1
@@ -6,32 +6,42 @@
 Common script, do not call it directly.
 #>
 
-if ($headers) {
-    Exit
-}
-
-Try {
-    # Install latest AD client library
-    # check for cloudshell https://shell.azure.com
-
-    if (!([microsoft.identity.client.azureCloudInstance])) {
-        $nuget = "nuget.exe"
-        if (!(test-path $nuget)) {
-            $nugetDownloadUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-            invoke-webRequest $nugetDownloadUrl -outFile $nuget
-        }
-
-        $env:path = $env:path.replace(";$pwd;", "") + ";$pwd;"
-        $ADPackage = "Microsoft.IdentityModel.Clients.ActiveDirectory"
-        & nuget.exe install $ADPackage > nuget.log
-
-        # Target .NET Framework version of the DLL
-        $FilePath = (Get-Item .\\Microsoft.IdentityModel.Clients.ActiveDirectory.[0-9].[0-9].[0-9]\\lib\\net[0-9][0-9]\\Microsoft.IdentityModel.Clients.ActiveDirectory.dll).FullName | Resolve-Path -Relative
-        Add-Type -Path $FilePath
+function main () {
+    if ($headers) {
+        return
     }
+
+    try {
+        # Install latest AD client library
+        # check for cloudshell https://shell.azure.com
+
+        if (!(get-cloudInstance)) {
+            $nuget = "nuget.exe"
+            if (!(test-path $nuget)) {
+                $nugetDownloadUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+                invoke-webRequest $nugetDownloadUrl -outFile $nuget
+            }
+
+            $env:path = $env:path.replace(";$pwd;", "") + ";$pwd;"
+            $ADPackage = "Microsoft.IdentityModel.Clients.ActiveDirectory"
+            & nuget.exe install $ADPackage > nuget.log
+
+            # Target .NET Framework version of the DLL
+            $FilePath = (Get-Item .\\Microsoft.IdentityModel.Clients.ActiveDirectory.[0-9].[0-9].[0-9]\\lib\\net[0-9][0-9]\\Microsoft.IdentityModel.Clients.ActiveDirectory.dll).FullName | Resolve-Path -Relative
+            Add-Type -Path $FilePath
+        }
+    }
+    catch {
+        Write-Warning $_.Exception.Message
+    }
+
+    return GetRESTHeaders
 }
-Catch {
-    Write-Warning $_.Exception.Message
+
+function get-cloudInstance() {
+    $isCloudInstance = $PSVersionTable.Platform -ieq 'unix' -and ($env:ACC_CLOUD)
+    write-host "cloud instance: $isCloudInstance"
+    return $isCloudInstance
 }
 
 function GetRESTHeaders() {
@@ -39,7 +49,7 @@ function GetRESTHeaders() {
     $clientId = "1950a258-227b-4e31-a9cf-717495945fc2"
     $redirectUrl = "urn:ietf:wg:oauth:2.0:oob"
     
-    if ([microsoft.identity.client.azureCloudInstance]) {
+    if (get-cloudInstance) {
         $token = GetRESTHeadersCloud
     }
     else {
@@ -47,10 +57,11 @@ function GetRESTHeaders() {
     }
     
     $authHeader = @{
-        'Content-Type'='application/json'
-        'Authorization'='Bearer ' + $token
+        'Content-Type'  = 'application/json'
+        'Authorization' = 'Bearer ' + $token
     }
 
+    write-host "auth header: $($authHeader | convertto-json)"
     return $authHeader
 }
 
@@ -81,7 +92,7 @@ function GetRESTHeadersCloud() {
     $response = invoke-webRequest -method post `
         -uri 'http://localhost:50342/oauth2/token' `
         -body "resource=$resourceUrl" `
-        -header @{'metadata'='true' }
+        -header @{'metadata' = 'true' }
 
     write-host $response | convertto-json
     $token = ($response | convertfrom-json).access_token
@@ -106,7 +117,7 @@ switch ($Location) {
     }
 }
 
-$headers = GetRESTHeaders
+$headers = main
 
 if ($ClusterName) {
     $WebApplicationName = $ClusterName + "_Cluster"
