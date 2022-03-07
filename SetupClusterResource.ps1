975dbe7d7ea7d1bbf2fe8019c87fde539cb54ef9
jason gilbertson <jagilber@microsoft.com>
2022-03-07 09:44:39 -0500

    testing #draft#

---
+++ SetupClusterResource.ps1
@@ -0,0 +1,117 @@
+<#
+.SYNOPSIS
+adds 'azureActiveDirectory' json configuration to given Microsoft.ServiceFabric/clusters resource
+
+requires azure module 'az'
+install-module az
+v 1.0
+
+.EXAMPLE
+.\setupClusterResource.ps1 -tenantId cb4dc457-01c8-40c7-855a-5468ebd5bc74 `
+    -clusterApplication e6bba8d5-3fb6-47ac-9927-2a50cf763d36 `
+    -clientApplication 2324af84-f14b-4ee8-a695-6b464f8dbb92 `
+    -resourceGroupName 'mysftestcluster' `
+    -clusterResource '/subscriptions/{{subscriptionId}}/resourcegroups/{{resourcegroup}}/providers/Microsoft.ServiceFabric/clusters/{{clusterName}}'
+#>
+
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$tenantId = '', # guid
+    [Parameter(Mandatory = $true)]
+    [string]$clusterApplication = '', # guid
+    [Parameter(Mandatory = $true)]
+    [string]$clientApplication = '', # guid
+    [Parameter(Mandatory = $true)]
+    [string]$resourceGroupName = '',
+    [Parameter(Mandatory = $true)]
+    [string]$clusterResource = '',
+    [string]$deploymentName = "add-azureactivedirectory-$((get-date).tostring('yy-MM-dd-HH-mm-ss'))",
+    [string]$templateFile = "$pwd\template-$deploymentName.json",
+    [switch]$whatIf,
+    [switch]$force
+)
+
+$azureActiveDirectory = @{ 
+    azureActiveDirectory = @{
+        tenantId           = $tenantId
+        clusterApplication = $clusterApplication
+        clientApplication  = $clientApplication
+    }
+}
+
+$aadNode = 'azureActiveDirectory'
+$azureActiveDirectory | convertto-json
+
+if ((test-path $templateFile) -and $force) {
+    remove-item $templateFile -Force
+}
+
+if (!(Get-AzContext)) { Connect-AzAccount }
+
+Export-AzResourceGroup -ResourceGroupName $resourceGroupName `
+    -Path $templateFile `
+    -Resource $clusterResource `
+    -IncludeComments `
+    -SkipAllParameterization `
+    -Verbose
+
+# modify template file
+$templateJson = get-content -raw $templateFile
+$templateJson
+$global:templateObj = $templateJson | convertfrom-json 
+$clusterObj = $global:templateObj.resources | Where-Object type -ieq 'Microsoft.ServiceFabric/clusters'
+
+if(!$clusterObj){
+    write-error "unable to find cluster resource"
+    return
+}
+
+if (!($clusterObj.properties.azureActiveDirectory)) {
+    write-host "adding $azureActiveDirectory"
+    Add-Member -InputObject $clusterObj.properties `
+        -TypeName System.Management.Automation.PSCustomObject `
+        -NotePropertyMembers $azureActiveDirectory
+}
+else {
+    write-warning "modifying $($clusterObj.properties.azureActiveDirectory)"
+    $clusterObj.properties.azureActiveDirectory = $azureActiveDirectory
+}
+
+$templateJson = $global:templateObj | convertto-json -depth 99
+$templateJson
+
+$templateJson | out-file $templateFile
+
+$ret = Test-azResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -TemplateFile $templateFile `
+    -Mode Complete
+
+if ($ret) {
+    Write-Error "template validation failed. error: `n`n$($ret.Code)`n`n$($ret.Message)`n`n$($ret.Details)"
+    return
+}
+else {
+    write-host "template valid: $templateFile" -ForegroundColor Green
+}
+
+write-host "New-AzResourceGroupDeployment -Name `"$deploymentName`" ``
+    -ResourceGroupName $resourceGroupName ``
+    -Mode Incremental ``
+    -DeploymentDebugLogLevel All ``
+    -TemplateFile $templateFile ``
+    -Verbose
+" -ForegroundColor Yellow
+
+if (!$whatIf) {
+    New-AzResourceGroupDeployment -Name "$deploymentName" `
+        -ResourceGroupName $resourceGroupName `
+        -Mode Incremental `
+        -DeploymentDebugLogLevel All `
+        -TemplateFile $templateFile `
+        -Verbose
+}
+else {
+    write-host "execute command above or rerun script without -whatif when ready to update cluster." -ForegroundColor Green
+}
+
+write-host "finished"
