913a33c15d250a470d2fb37408eb74dc6db5dd40
jason gilbertson <jagilber@microsoft.com>
2022-04-20 09:50:56 -0400

    modify arguments for consistency. add support for managed clusters

--- SetupClusterResource.ps1
+++ SetupClusterResource.ps1
@@ -4,14 +4,14 @@ adds 'azureActiveDirectory' json configuration to given Microsoft.ServiceFabric/
 
 requires azure module 'az'
 install-module az
-v 1.0
+v 1.1
 
 .EXAMPLE
 .\setupClusterResource.ps1 -tenantId cb4dc457-01c8-40c7-855a-5468ebd5bc74 `
     -clusterApplication e6bba8d5-3fb6-47ac-9927-2a50cf763d36 `
     -clientApplication 2324af84-f14b-4ee8-a695-6b464f8dbb92 `
     -resourceGroupName 'mysftestcluster' `
-    -clusterResource '/subscriptions/{{subscriptionId}}/resourcegroups/{{resourcegroup}}/providers/Microsoft.ServiceFabric/clusters/{{clusterName}}'
+    -clusterName 'mysftestcluster'
 #>
 
 param(
@@ -24,12 +24,15 @@ param(
     [Parameter(Mandatory = $true)]
     [string]$resourceGroupName = '',
     [Parameter(Mandatory = $true)]
-    [string]$clusterResource = '',
+    [string]$clusterName = '',
     [string]$deploymentName = "add-azureactivedirectory-$((get-date).tostring('yy-MM-dd-HH-mm-ss'))",
     [string]$templateFile = "$pwd\template-$deploymentName.json",
     [switch]$whatIf,
     [switch]$force
 )
+
+$PSModuleAutoLoadingPreference = 2
+#$ErrorActionPreference = 'continue'
 
 $azureActiveDirectory = @{ 
     azureActiveDirectory = @{
@@ -39,14 +42,40 @@ $azureActiveDirectory = @{
     }
 }
 
-$aadNode = 'azureActiveDirectory'
 $azureActiveDirectory | convertto-json
 
 if ((test-path $templateFile) -and $force) {
     remove-item $templateFile -Force
 }
 
+if (!(get-command get-azcontext)) {
+    write-warning "azure az module is required to run this script. 'ctrl-c' to exit script or {enter} to continue."
+    if (!$force) {
+        pause
+    }
+    install-module az -Force -AllowClobber
+}
+
 if (!(Get-AzContext)) { Connect-AzAccount }
+
+$resources = @(get-azresource -Name $clusterName | Where-Object ResourceType -imatch 'Microsoft.ServiceFabric')
+
+if ($resources.Count -ne 1) {
+    write-error "unable to find cluster $(resources)"
+    return
+}
+
+$clusterResource = $resources[0].ResourceId
+$resourceType = $resources[0].ResourceType
+
+write-host "
+Export-AzResourceGroup -ResourceGroupName $resourceGroupName ``
+    -Path $templateFile ``
+    -Resource $clusterResource ``
+    -IncludeComments ``
+    -SkipAllParameterization ``
+    -Verbose
+"
 
 Export-AzResourceGroup -ResourceGroupName $resourceGroupName `
     -Path $templateFile `
@@ -59,9 +88,9 @@ Export-AzResourceGroup -ResourceGroupName $resourceGroupName `
 $templateJson = get-content -raw $templateFile
 $templateJson
 $global:templateObj = $templateJson | convertfrom-json 
-$clusterObj = $global:templateObj.resources | Where-Object type -ieq 'Microsoft.ServiceFabric/clusters'
+$clusterObj = $global:templateObj.resources | Where-Object type -ieq $resourceType
 
-if(!$clusterObj){
+if (!$clusterObj) {
     write-error "unable to find cluster resource"
     return
 }
@@ -81,6 +110,12 @@ $templateJson = $global:templateObj | convertto-json -depth 99
 $templateJson
 
 $templateJson | out-file $templateFile
+
+write-host "
+Test-azResourceGroupDeployment -ResourceGroupName $resourceGroupName ``
+    -TemplateFile $templateFile ``
+    -Mode Complete
+"
 
 $ret = Test-azResourceGroupDeployment -ResourceGroupName $resourceGroupName `
     -TemplateFile $templateFile `
@@ -114,4 +149,5 @@ else {
     write-host "execute command above or rerun script without -whatif when ready to update cluster." -ForegroundColor Green
 }
 
+write-host "updated template file: $($templateFile)" -ForegroundColor Cyan
 write-host "finished"
