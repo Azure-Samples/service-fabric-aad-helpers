e5e20adfb98c43915278359ee09f2c1ab7da04c5
jagilber <jagilber@microsoft.com>
2024-01-14 12:18:36 -0500

    add microsoft graph client id, secret and granttype parameters for testing. tested

--- test-aad-setup.ps1
+++ test-aad-setup.ps1
@@ -7,43 +7,97 @@ param(
     [string]$resourceGroupName,
     [string]$tenantId = "$((get-azcontext).tenant.id)",
     [string]$clusterName = $resourceGroupName,
-    [string]$translog = "$pwd/tran-$($startTime.tostring('yyMMddhhmmss')).log",
     [switch]$setupReadonlyUser,
     [switch]$setupAdminUser,
     [switch]$setupClusterResource,
     [switch]$remove,
     [switch]$force,
-    [switch]$addVisualStudioAccess
+    [switch]$addVisualStudioAccess,
+    [guid]$MGClientId = '14d82eec-204b-4c2f-b7e8-296a70dab67e', # well-known ps graph client id generated on connect
+    [string]$MGClientSecret = $null,
+    [string]$MGGrantType = 'urn:ietf:params:oauth:grant-type:device_code' #'client_credentials', #'authorization_code'
 )
 
 $errorActionPreference = 'continue'
 $curDir = $pwd
 $startTime = get-date
+$replyUrl = ""
+$webApplicationUri = ""
 
-try {
-    write-host "$(get-date) starting transcript $translog"
-    $location = (get-azResourceGroup -Name $resourceGroupName).location
-    if (!$location) {
-        throw "resource group $resourceGroupName not found"
-    }
+function main() {
+    try {
 
-    $replyUrl = "https://$clusterName.$location.cloudapp.azure.com:19080/Explorer/index.html" # <--- client browser redirect url
+        $location = (get-azResourceGroup -Name $resourceGroupName).location
+        if (!$location) {
+            throw "resource group $resourceGroupName not found"
+        }
+        $replyUrl = "https://$clusterName.$location.cloudapp.azure.com:19080/Explorer/index.html" # <--- client browser redirect url
 
-    #$webApplicationUri = 'https://mysftestcluster.contoso.com' # <--- must be verified domain due to AAD changes
-    $webApplicationUri = "api://$tenantId/$clusterName" # <--- does not have to be verified domain
+        #$webApplicationUri = 'https://mysftestcluster.contoso.com' # <--- must be verified domain due to AAD changes
+        $webApplicationUri = "api://$tenantId/$clusterName" # <--- does not have to be verified domain
+            
+        if ($MGClientSecret) {
+            setup-applicationMG
+
+            write-host "ConfigObj:" -ForegroundColor Cyan
+            $ConfigObj
     
+            if ($setupReadonlyUser) {
+                setup-readOnlyUserMG
+            }
+            if ($setupAdminUser) {
+                setup-adminUserMG
+            }
+
+            write-host "ConfigObj:" -ForegroundColor Cyan
+            $ConfigObj
+    
+        }
+        else {
+            setup-application
+
+            write-host "ConfigObj:" -ForegroundColor Cyan
+            $ConfigObj
+    
+            if ($setupReadonlyUser) {
+                setup-readOnlyUser
+            }
+            if ($setupAdminUser) {
+                setup-adminUser
+            }
+
+            write-host "ConfigObj:" -ForegroundColor Cyan
+            $ConfigObj
+    
+        }
+    
+        if ($setupClusterResource) {
+            write-host ".\SetupClusterResource.ps1 -resourceGroupName $resourceGroupName
+            -ConfigObj $($ConfigObj | convertto-json -depth 99)
+        " -ForegroundColor Cyan
+
+            .\SetupClusterResource.ps1 -configObj $ConfigObj -resourceGroupName $resourceGroupName
+        }
+    }
+    finally {
+        set-location $curDir
+    }
+}
+
+function setup-application() {
     $translog = "$pwd/setup-application-$($startTime.tostring('yyMMddhhmmss')).log"
+
     write-host ".\SetupApplications.ps1 -TenantId $tenantId ``
-        -ClusterName $clusterName ``
-        -SpaApplicationReplyUrl $replyUrl ``
-        -AddResourceAccess ``
-        -WebApplicationUri $webApplicationUri ``
-        -logFile $translog ``
-        -AddVisualStudioAccess:`$$addVisualStudioAccess ``
-        -Verbose ``
-        -force:`$$force ``
-        -remove:`$$remove
-    " -ForegroundColor Cyan
+    -ClusterName $clusterName ``
+    -SpaApplicationReplyUrl $replyUrl ``
+    -AddResourceAccess ``
+    -WebApplicationUri $webApplicationUri ``
+    -logFile $translog ``
+    -AddVisualStudioAccess:`$$addVisualStudioAccess ``
+    -Verbose ``
+    -force:`$$force ``
+    -remove:`$$remove
+" -ForegroundColor Cyan
 
     $ConfigObj = .\SetupApplications.ps1 -TenantId $tenantId `
         -ClusterName $clusterName `
@@ -55,62 +109,143 @@ try {
         -Verbose `
         -force:$force `
         -remove:$remove
-
-    write-host "ConfigObj:" -ForegroundColor Cyan
-    $ConfigObj
-
-    if ($setupReadonlyUser) {
-        $translog = "$pwd/setup-testuser-$($startTime.tostring('yyMMddhhmmss')).log"
-        write-host ".\SetupUser.ps1 -UserName 'TestUser' ``
-        -Password 'P@ssword!123' ``
-        -Verbose ``
-        -logFile $translog ``
-        -remove:`$$remove ``
-        -force:`$$force ``
-        -ConfigObj $($ConfigObj | convertto-json -depth 99)
-    " -ForegroundColor Cyan
-
-        .\SetupUser.ps1 -ConfigObj $ConfigObj `
-            -UserName 'TestUser' `
-            -Password 'P@ssword!123' `
-            -Verbose `
-            -logFile $translog `
-            -remove:$remove `
-            -force:$force
-    }
-    
-    if ($setupAdminUser) {
-        $translog = "$pwd/setup-testadmin-$($startTime.tostring('yyMMddhhmmss')).log"
-        write-host ".\SetupUser.ps1 -UserName 'TestAdmin' ``
-            -Password 'P@ssword!123' ``
-            -IsAdmin ``
-            -Verbose ``
-            -logFile $translog ``
-            -remove:`$$remove ``
-            -force:`$$force
-            -ConfigObj $($ConfigObj | convertto-json -depth 99)
-        " -ForegroundColor Cyan
-    
-        .\SetupUser.ps1 -ConfigObj $ConfigObj `
-            -UserName 'TestAdmin' `
-            -Password 'P@ssword!123' `
-            -IsAdmin `
-            -Verbose `
-            -logFile $translog `
-            -remove:$remove `
-            -force:$force    
-    }
-
-    if ($setupClusterResource) {
-        write-host ".\SetupClusterResource.ps1 -resourceGroupName $resourceGroupName
-            -ConfigObj $($ConfigObj | convertto-json -depth 99)
-        " -ForegroundColor Cyan
-
-        .\SetupClusterResource.ps1 -configObj $ConfigObj -resourceGroupName $resourceGroupName
-    }
 }
-finally {
-    write-host "$(get-date) stopping transcript $translog"
-    #stop-transcript
-    set-location $curDir
+
+function setup-applicationMG() {
+    $translog = "$pwd/setup-application-$($startTime.tostring('yyMMddhhmmss')).log"
+
+    write-host ".\SetupApplications.ps1 -TenantId $tenantId ``
+    -ClusterName $clusterName ``
+    -SpaApplicationReplyUrl $replyUrl ``
+    -AddResourceAccess ``
+    -WebApplicationUri $webApplicationUri ``
+    -logFile $translog ``
+    -AddVisualStudioAccess:`$$addVisualStudioAccess ``
+    -Verbose ``
+    -force:`$$force ``
+    -remove:`$$remove ``
+    -MGClientId $MGClientId ``
+    -MGClientSecret $MGClientSecret ``
+    -MGGrantType $MGGrantType
+" -ForegroundColor Cyan
+
+    $ConfigObj = .\SetupApplications.ps1 -TenantId $tenantId `
+        -ClusterName $clusterName `
+        -SpaApplicationReplyUrl $replyUrl `
+        -AddResourceAccess `
+        -WebApplicationUri $webApplicationUri `
+        -AddVisualStudioAccess:$addVisualStudioAccess `
+        -logFile $translog `
+        -Verbose `
+        -force:$force `
+        -remove:$remove `
+        -MGClientId $MGClientId `
+        -MGClientSecret $MGClientSecret `
+        -MGGrantType $MGGrantType
 }
+
+function setup-readOnlyUser() {
+    $translog = "$pwd/setup-readonly-$($startTime.tostring('yyMMddhhmmss')).log"
+
+    write-host ".\SetupUser.ps1 -UserName 'TestReadOnly' ``
+    -Password 'P@ssword!123' ``
+    -IsReadOnly ``
+    -Verbose ``
+    -logFile $translog ``
+    -remove:`$$remove ``
+    -force:`$$force
+    -ConfigObj $($ConfigObj | convertto-json -depth 99)
+" -ForegroundColor Cyan
+    
+    .\SetupUser.ps1 -ConfigObj $ConfigObj `
+        -UserName 'TestReadOnly' `
+        -Password 'P@ssword!123' `
+        -IsReadOnly `
+        -Verbose `
+        -logFile $translog `
+        -remove:$remove `
+        -force:$force
+}
+
+function setup-readOnlyUserMG() {
+    $translog = "$pwd/setup-readonly-$($startTime.tostring('yyMMddhhmmss')).log"
+
+    write-host ".\SetupUser.ps1 -UserName 'TestReadOnly' ``
+    -Password 'P@ssword!123' ``
+    -IsReadOnly ``
+    -Verbose ``
+    -logFile $translog ``
+    -remove:`$$remove ``
+    -force:`$$force
+    -ConfigObj $($ConfigObj | convertto-json -depth 99)
+    -MGClientId $MGClientId `
+    -MGClientSecret $MGClientSecret `
+    -MGGrantType $MGGrantType
+" -ForegroundColor Cyan
+        
+    .\SetupUser.ps1 -ConfigObj $ConfigObj `
+        -UserName 'TestReadOnly' `
+        -Password 'P@ssword!123' `
+        -IsReadOnly `
+        -Verbose `
+        -logFile $translog `
+        -remove:$remove `
+        -force:$force `
+        -MGClientId $MGClientId `
+        -MGClientSecret $MGClientSecret `
+        -MGGrantType $MGGrantType
+}
+
+function setup-adminUser() {
+    $translog = "$pwd/setup-admin-$($startTime.tostring('yyMMddhhmmss')).log"
+
+    write-host ".\SetupUser.ps1 -UserName 'TestAdmin' ``
+    -Password 'P@ssword!123' ``
+    -IsAdmin ``
+    -Verbose ``
+    -logFile $translog ``
+    -remove:`$$remove ``
+    -force:`$$force
+    -ConfigObj $($ConfigObj | convertto-json -depth 99)
+" -ForegroundColor Cyan
+    
+    .\SetupUser.ps1 -ConfigObj $ConfigObj `
+        -UserName 'TestAdmin' `
+        -Password 'P@ssword!123' `
+        -IsAdmin `
+        -Verbose `
+        -logFile $translog `
+        -remove:$remove `
+        -force:$force
+}
+
+function setup-adminUserMG() {
+    $translog = "$pwd/setup-admin-$($startTime.tostring('yyMMddhhmmss')).log"
+
+    write-host ".\SetupUser.ps1 -UserName 'TestAdmin' ``
+    -Password 'P@ssword!123' ``
+    -IsAdmin ``
+    -Verbose ``
+    -logFile $translog ``
+    -remove:`$$remove ``
+    -force:`$$force
+    -ConfigObj $($ConfigObj | convertto-json -depth 99)
+    -MGClientId $MGClientId `
+    -MGClientSecret $MGClientSecret `
+    -MGGrantType $MGGrantType
+" -ForegroundColor Cyan
+    
+    .\SetupUser.ps1 -ConfigObj $ConfigObj `
+        -UserName 'TestAdmin' `
+        -Password 'P@ssword!123' `
+        -IsAdmin `
+        -Verbose `
+        -logFile $translog `
+        -remove:$remove `
+        -force:$force `
+        -MGClientId $MGClientId `
+        -MGClientSecret $MGClientSecret `
+        -MGGrantType $MGGrantType
+}
+
+main
